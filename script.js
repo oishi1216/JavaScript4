a8473cb21b3a4d1e35e605af524f87674af57082
oishi <abdaqoopmhm@gmail.com>
2021-03-11 00:39:57 +0900

    提出2回目

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ const buttonElement =  document.getElementById('button');
 const answerDiv = document.getElementById('answer');
 const goHome = document.getElementById('gohome');
 
+class Quiz {
+  constructor(category, difficulty, question, correct_answer, incorrect_answer1, incorrect_answer2, incorrect_answer3){
+    this.category = category;
+    this.difficulty = difficulty;
+    this.question = question;
+    this.correct_answer = correct_answer;
+    this.incorrect_answer1 = incorrect_answer1;
+    this.incorrect_answer2 = incorrect_answer2;
+    this.incorrect_answer3 = incorrect_answer3;
+  }
+}
+
 let correctNum = 0;
 
 goHome.style.display = "none";
@@ -18,27 +30,39 @@ const quizLoad = () => {
   question.innerHTML = '少々お待ちください';
   startButton.style.display = "none";
 
-  fetch(quizAPI)
-  .then((response) => response.json())
-  .then((jsonData) => {
-    const quiz = jsonData.results;
-    console.log(quiz);
-    let quizNum = 1;
-    nextQizu(quiz, quizNum);
 
+  async function getQuiz() {
+    const response = await fetch(quizAPI);
+    const jsonData = await response.json();
+    return jsonData;
+  }
+  getQuiz()
+    .then(jsonData => {
+      const quiz = jsonData.results;
+
+      let quizNum = 1;
+      let quizClass = new Quiz( quiz[quizNum - 1].category, quiz[quizNum - 1].difficulty, quiz[quizNum - 1].question, quiz[quizNum - 1].correct_answer, quiz[quizNum - 1].incorrect_answers[0], quiz[quizNum - 1].incorrect_answers[1], quiz[quizNum - 1].incorrect_answers[2]);
+
+      nextQizu(quizClass, quizNum);
+
+    })
+
+  .catch(e => {
+    console.log(e.message);
   });
+
 };
 
-const nextQizu = (quiz, quizNum) => {
+const nextQizu = (quizClass, quizNum) => {
   header.innerHTML = `問題${quizNum}`;
-  category.innerHTML = `[ジャンル]${quiz[quizNum - 1].category}`;
-  difficulty.innerHTML = `[難易度]${quiz[quizNum - 1].difficulty}`;
-  question.innerHTML = `${quiz[quizNum - 1].question}`;
+  category.innerHTML = `[ジャンル]${quizClass.category}`;
+  difficulty.innerHTML = `[難易度]${quizClass.difficulty}`;
+  question.innerHTML = `${quizClass.question}`;
 
-  const correctAnswer = quiz[quizNum - 1].correct_answer;
-  const incorrectAnswer1 = quiz[quizNum - 1].incorrect_answers[0];
-  const incorrectAnswer2 = quiz[quizNum - 1].incorrect_answers[1];
-  const incorrectAnswer3 = quiz[quizNum - 1].incorrect_answers[2];
+  const correctAnswer = quizClass.correct_answer;
+  const incorrectAnswer1 = quizClass.incorrect_answer1;
+  const incorrectAnswer2 = quizClass.incorrect_answer2;
+  const incorrectAnswer3 = quizClass.incorrect_answer3;
 
   let Answer = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3];
   let shuffleAnswer = shuffle(Answer);
@@ -54,21 +78,21 @@ const nextQizu = (quiz, quizNum) => {
       if (value === correctAnswer){
         correctNum++;
       }
-      nextButton(answerDiv, quiz, quizNum);
+      nextButton(answerDiv, quizClass, quizNum);
     });
   });
+};
 
-const nextButton = (answerDiv, quiz, quizNum) => {
+const nextButton = (answerDiv, quizClass, quizNum) => {
   quizNum++;
   while(answerDiv.firstChild){
     answerDiv.removeChild(answerDiv.firstChild);
   };
 
   if (quizNum <11) {
-    nextQizu(quiz, quizNum);
+    nextQizu(quizClass, quizNum);
   } else {
     header.innerHTML = `あなたの正答数は${correctNum}です！！`;
-    console.log(correctNum);
     category.innerHTML = '';
     difficulty.innerHTML = '';
     question.innerHTML = '再度チャレンジしたい場合は以下をクリック！！';
