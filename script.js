2b2dc08d7ff3bb68a3a2f07e4b68e8382908e29f
oishi <abdaqoopmhm@gmail.com>
2021-03-12 16:02:26 +0900

    提出3回目

--- script.js
+++ script.js
@@ -10,89 +10,105 @@ const answerDiv = document.getElementById('answer');
 const goHome = document.getElementById('gohome');
 
 class Quiz {
-  constructor(category, difficulty, question, correct_answer, incorrect_answer1, incorrect_answer2, incorrect_answer3){
-    this.category = category;
-    this.difficulty = difficulty;
-    this.question = question;
-    this.correct_answer = correct_answer;
-    this.incorrect_answer1 = incorrect_answer1;
-    this.incorrect_answer2 = incorrect_answer2;
-    this.incorrect_answer3 = incorrect_answer3;
-  }
+  constructor(quizData) {
+      this._quizzes = quizData.results;
+      this._correctAnswersNum = 0;
+    }
+  
+    //Get API data elements
+    getQuizCategory(index) {
+      return this._quizzes[index - 1].category;
+    }
+  
+    getQuizDifficulty(index) {
+      return this._quizzes[index - 1].difficulty;
+    }
+  
+    getQuizQuestion(index) {
+      return this._quizzes[index - 1].question;
+    }
+  
+    //Quiz Length
+    getNumQuizzes() {
+      return this._quizzes.length;
+    }
+  
+    //Get CorrectAnswer
+    getCorrectAnswer(index) {
+      return this._quizzes[index - 1].correct_answer;
+    }
+  
+    getIncorrectAnswers(index) {
+      return this._quizzes[index - 1].incorrect_answers;
+    }
+  
+    //CorrectAnswerCount
+    countCorrectAnswers(index, answer) {
+      const correctAnswer = this._quizzes[index - 1].correct_answer;
+      if (answer === correctAnswer) {
+        return this._correctAnswersNum++;
+      }
+    }
+  
+    //return CorrectAnswer
+    getCorrectAnswersNum() {
+      return this._correctAnswersNum;
+    }
 }
-
-let correctNum = 0;
 
 goHome.style.display = "none";
 
-const quizLoad = () => {
+const quizLoad = async (index) => {
   header.innerHTML = '取得中';
   question.innerHTML = '少々お待ちください';
   startButton.style.display = "none";
-
-
-  async function getQuiz() {
+  try {
     const response = await fetch(quizAPI);
-    const jsonData = await response.json();
-    return jsonData;
-  }
-  getQuiz()
-    .then(jsonData => {
-      const quiz = jsonData.results;
+    const quizData = await response.json();
+    const quiz = new Quiz(quizData);
 
-      let quizNum = 1;
-      let quizClass = new Quiz( quiz[quizNum - 1].category, quiz[quizNum - 1].difficulty, quiz[quizNum - 1].question, quiz[quizNum - 1].correct_answer, quiz[quizNum - 1].incorrect_answers[0], quiz[quizNum - 1].incorrect_answers[1], quiz[quizNum - 1].incorrect_answers[2]);
-
-      nextQizu(quizClass, quizNum);
-
-    })
-
-  .catch(e => {
+    nextQizu(quiz, index);
+  }  catch(e) {
     console.log(e.message);
-  });
-
+  };
 };
 
-const nextQizu = (quizClass, quizNum) => {
-  header.innerHTML = `問題${quizNum}`;
-  category.innerHTML = `[ジャンル]${quizClass.category}`;
-  difficulty.innerHTML = `[難易度]${quizClass.difficulty}`;
-  question.innerHTML = `${quizClass.question}`;
+const nextQizu = (quiz, index) => {
+  header.innerHTML = `問題${index}`;
+  category.innerHTML = `[ジャンル]${quiz.getQuizCategory(index)}`;
+  difficulty.innerHTML = `[難易度]${quiz.getQuizDifficulty(index)}`;
+  question.innerHTML = `${quiz.getQuizQuestion(index)}`;
 
-  const correctAnswer = quizClass.correct_answer;
-  const incorrectAnswer1 = quizClass.incorrect_answer1;
-  const incorrectAnswer2 = quizClass.incorrect_answer2;
-  const incorrectAnswer3 = quizClass.incorrect_answer3;
+  const correctAnswer = quiz.getCorrectAnswer(index);
+  const incorrectAnswer = quiz.getIncorrectAnswers(index) ;
 
-  let Answer = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3];
+  let Answer = [correctAnswer, ...incorrectAnswer];
   let shuffleAnswer = shuffle(Answer);
 
-  shuffleAnswer.forEach((value) => {
+  shuffleAnswer.forEach((answer) => {
     const answerButton =  document.createElement('button');
-    answerButton.textContent = value;
+    answerButton.textContent = answer;
     const answerP = document.createElement('p');
     answerP.appendChild(answerButton);
     answerDiv.appendChild(answerP);
 
     answerButton.addEventListener('click', () => {
-      if (value === correctAnswer){
-        correctNum++;
-      }
-      nextButton(answerDiv, quizClass, quizNum);
+      quiz.countCorrectAnswers(index, answer)
+      nextButton(answerDiv, quiz, index);
     });
   });
 };
 
-const nextButton = (answerDiv, quizClass, quizNum) => {
-  quizNum++;
+const nextButton = (answerDiv, quiz, index) => {
+  index++;
   while(answerDiv.firstChild){
     answerDiv.removeChild(answerDiv.firstChild);
   };
 
-  if (quizNum <11) {
-    nextQizu(quizClass, quizNum);
+  if (index <= quiz.getNumQuizzes()) {
+    nextQizu(quiz, index);
   } else {
-    header.innerHTML = `あなたの正答数は${correctNum}です！！`;
+    header.innerHTML = `あなたの正答数は${quiz.getCorrectAnswersNum()}です！！`;
     category.innerHTML = '';
     difficulty.innerHTML = '';
     question.innerHTML = '再度チャレンジしたい場合は以下をクリック！！';
@@ -102,8 +118,6 @@ const nextButton = (answerDiv, quizClass, quizNum) => {
       question.innerHTML = '以下のボタンをクリック';
       goHome.style.display = "none";
       startButton.style.display = "";
-      quizNum = 0;
-      correctNum = 0;
     });
   }
 }
@@ -116,4 +130,4 @@ const shuffle = ([...array]) => {
   return array;
 }
 
-startButton.addEventListener('click', () => quizLoad());
+startButton.addEventListener('click', () => quizLoad(1));
